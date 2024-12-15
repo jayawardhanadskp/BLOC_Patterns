3c788f42c911245ded638feb90840187a09f6934
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-16 00:12:52 +0530

    auth event

---
+++ login_app_bloc/lib/bloc/auth_event.dart
@@ -0,0 +1,13 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthEvent {}
+
+final class AuthLogInRequested extends AuthEvent {
+  final String email;
+  final String password;
+
+  AuthLogInRequested({required this.email, required this.password});
+}
+
+final class AuthLogOutRequested extends AuthEvent {}  
